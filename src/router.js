3e52cb2f7c4ada8eb195bfe55f64700cd0b2020a
helloyoogs <helloyoogs@naver.com>
2023-04-25 15:20:12 +0900

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import {createRouter, createWebHistory} from "vue-router";
+import IdCard from './components/IdCard.vue';
+import main from './components/Main.vue';
+import MainLoad from './components/MainLoad.vue';
+import MovingPhotos from './components/MovingPhotos.vue';
+import boost from './components/boost.vue';
+import images from './components/images.vue';
+
+
+
+const router = createRouter({
+  mode: "history",
+  history: createWebHistory(),
+  routes: [
+      {
+          path: '/',
+          redirect: '/newjeans' // '/' 경로로 접근 시 '/newjeans' 경로로 리다이렉트
+      },
+    {
+      path: "/newjeans",
+      component: MainLoad,
+    },
+  
+        {
+          path: "/newjeans/id-card",
+          component: IdCard,
+        },
+        {
+          path: "/newjeans/main",
+          component: main,
+        },
+        {
+          path: "/newjeans/moving-photos",
+          component: MovingPhotos,
+        },
+        {
+          path: "/newjeans/boost",
+          component: boost,
+        },
+        {
+          path: "/newjeans/boost/images",
+          component: images,
+        },
+      ]
+});
+
+export default router; 
