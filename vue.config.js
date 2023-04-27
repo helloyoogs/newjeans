1adf537d6ef4111db84b11d7c619dcf71ec32b9b
helloyoogs <helloyoogs@naver.com>
2023-04-27 10:43:47 +0900

    호스팅 연결 진행중

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/newjeans',
+  publicPath: '/newjeans/',
   outputDir: 'docs', // 깃허브 페이지에서 사용할 파일이 생성되는 경로
 })
